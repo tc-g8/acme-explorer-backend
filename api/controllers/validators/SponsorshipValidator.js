870bd266c7ed0433d7a308e1c5ab62590a0bc8e0
Mario Ruano <mrf1989@hotmail.com>
2023-05-01 20:38:37 +0200

    fix: update sponsorship validator

--- api/controllers/validators/SponsorshipValidator.js
+++ api/controllers/validators/SponsorshipValidator.js
@@ -1,45 +1,46 @@
 import { check } from "express-validator";
 
 const creationSponsorshipValidator = [
-    check("banner")
-        .exists({ checkNull: true, checkFalsy: true }),
-    check("landingPage")
-        .exists({ checkNull: true, checkFalsy: true })
-        .withMessage("Landing page is required")
-        .isString()
-        .isURL()
-        .withMessage("Landing page must be an URL")
-        .trim()
-        .escape(),
-    check("amount")
-        .not()
-        .exists(),
-    check("status")
-        .not()
-        .exists(),
-    check("sponsor_id")
-        .exists({ checkNull: true, checkFalsy: true })
-        .withMessage("Sponsor id required")
-]
+  check("banner")
+    .exists({ checkNull: true, checkFalsy: true }),
+  check("landingPage")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage("Landing page is required")
+    .isString()
+    .isURL()
+    .withMessage("Landing page must be an URL")
+    .trim()
+    .escape(),
+  check("amount")
+    .not()
+    .exists(),
+  check("status")
+    .not()
+    .exists(),
+  check("sponsor_id")
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage("Sponsor id required")
+];
 
 const updateSponsorshipValidator = [
-    check("landingPage")
-        .optional()
-        .withMessage("Landing page can not be empty")
-        .isString()
-        .isURL()
-        .withMessage("Landing page must be an URL")
-        .trim()
-        .escape(),
-    check("amount")
-        .not()
-        .exists(),
-    check("status")
-        .not()
-        .exists(),
-    check("sponsor_id")
-        .not()
-        .exists()
-]
+  check("landingPage")
+    .optional()
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage("Landing page can not be empty")
+    .isString()
+    .isURL()
+    .withMessage("Landing page must be an URL")
+    .trim()
+    .escape(),
+  check("amount")
+    .not()
+    .exists(),
+  check("status")
+    .not()
+    .exists(),
+  check("sponsor_id")
+    .not()
+    .exists()
+];
 
 export { creationSponsorshipValidator, updateSponsorshipValidator };
